917afd5cda7f6d3920b63a5d9877d9e3d6fb8e1f
my-name <ajaygouda10@gmail.com>
2025-07-14 12:14:36 +0530

    adding changes

--- ui.frontend/src/main/webpack/components/React/dashboard/index.jsx
+++ ui.frontend/src/main/webpack/components/React/dashboard/index.jsx
@@ -5,18 +5,18 @@ import Navigation from "./navigation";
 import Grid from "./grid";
 import Linear from "./linear";
 import './dashboard.scss';
-// import pageIcon from './page.svg';
-// import warningIcon from './warning.svg';
-// import publishedIcon from './published.svg';
-// import unpublishedIcon from './unpublished.svg';
+// import pageIcon from '../../../../../../../ui.apps/src/main/content/jcr_root/apps/chd/clientlibs/clientlib-site/resources/images/page.svg';
+// import warningIcon from '../../../../../../../ui.apps/src/main/content/jcr_root/apps/chd/clientlibs/clientlib-site/resources/images/warning.svg';
+// import publishedIcon from '../../../../../../../ui.apps/src/main/content/jcr_root/apps/chd/clientlibs/clientlib-site/resources/images/published.svg';
+// import unpublishedIcon from '../../../../../../../ui.apps/src/main/content/jcr_root/apps/chd/clientlibs/clientlib-site/resources/images/unpublished.svg';
 
 
 const initialContentWidgets = [
-  { name: 'Total Pages', count: 0, icon: "pageIcon" },
-  { name: 'Published Pages', count: 0, icon: "publishedIcon" },
-  { name: 'Total Issues', count: 0, icon: "warningIcon" },
-  { name: 'Unpublished Pages', count: 0, icon: "unpublishedIcon" }
-]
+  { id: 1, name: 'Total Pages', key: 'totalPages', icon: <path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm0-80h420v-140H160v140Zm500 0h140v-360H660v360ZM160-460h420v-140H160v140Z"/> },
+  { id: 2, name: 'Published Pages', key: 'publishedPages', icon: <path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/> },
+  { id: 3, name: 'Total Issues', key: 'totalIssues', icon: <path d="m40-120 440-760 440 760H40Zm138-80h604L480-720 178-200Zm302-40q17 0 28.5-11.5T520-280q0-17-11.5-28.5T480-320q-17 0-28.5 11.5T440-280q0 17 11.5 28.5T480-240Zm-40-120h80v-200h-80v200Zm40-100Z"/>},
+  { id: 4, name: 'Unpublished Pages', key: 'unpublishedPages', icon: <path d="M819-28 701-146q-48 32-103.5 49T480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-62 17-117.5T146-701L27-820l57-57L876-85l-57 57ZM480-160q45 0 85.5-12t76.5-33L487-360l-63 64-170-170 56-56 114 114 7-8-226-226q-21 36-33 76.5T160-480q0 133 93.5 226.5T480-160Zm335-100-59-59q21-35 32.5-75.5T800-480q0-133-93.5-226.5T480-800q-45 0-85.5 11.5T319-756l-59-59q48-31 103.5-48T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 61-17 116.5T815-260ZM602-474l-56-56 104-104 56 56-104 104Zm-64-64ZM424-424Z"/> }
+];
 
 const status = [
   { name: 'Critical', color: 0 },
@@ -74,13 +74,20 @@ export default function Dashboard() {
   }
 
   const updateWidgets = (data) => {
-    setWidgets([
-      { name: 'Total Pages', count: data.totalPages || 0, icon: "pageIcon" },
-      { name: 'Published Pages', count: data.publishedPages || 0, icon: "publishedIcon" },
-      { name: 'Total Issues', count: data.pages.length, icon: "warningIcon" },
-      { name: 'Unpublished Pages', count: data.unpublishedPages || 0, icon: "unpublishedIcon" }
-    ])
-  }
+    setWidgets(
+      initialContentWidgets.map(widget => {
+        let count = 0;
+
+        if (widget.key === 'totalIssues') {
+          count = data.pages?.length || 0;
+        } else {
+          count = data[widget.key] || 0;
+        }
+
+        return { ...widget, count };
+      })
+    );
+  };
 
   const handleSearch = (value) => {
     setSearchKey(value);
@@ -116,6 +123,7 @@ export default function Dashboard() {
       setRowData(rawData)
     }
   }
+  console.log("widgets",widgets)
 
   return (
     <>
@@ -135,7 +143,7 @@ export default function Dashboard() {
           <div className="ch-dashboard__widgets">
             <div className="left">
               {widgets && widgets?.map((widget, index) => (
-                <Widget key={index} name={widget.name} count={widget.count} />
+                <Widget key={index} name={widget.name} count={widget.count} icon={widget.icon} />
               ))}
             </div>
             <div className="right">
--- ui.frontend/src/main/webpack/components/React/dashboard/widget.jsx
+++ ui.frontend/src/main/webpack/components/React/dashboard/widget.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 export default function Widget({key, name, count, icon}) {
+    console.log(icon)
     return (
         <div key={key} class="ch-dashboard__widget">
             <div class="ch-dashboard__widget-icon">
-                <img src={icon} alt="icon" />
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill=" #6D159F">{React.createElement(icon.type, icon.props)}</svg>
             </div>
             <div class="ch-dashboard__widget-content">
                 <p class="type">{name}</p>
