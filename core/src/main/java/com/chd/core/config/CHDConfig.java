f2c22b6e246089bf283a58fef64961a8910e19b0
Shilpadsouza07 <shilpadsouza07@gmail.com>
2025-07-21 19:15:30 +0530

    addition and deletion of unnecessary files

---
+++ core/src/main/java/com/chd/core/config/CHDConfig.java
@@ -0,0 +1,16 @@
+package com.chd.core.config;
+
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+import org.osgi.service.metatype.annotations.AttributeType;
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+
+@ObjectClassDefinition(name = "Content Health Dashboard Config")
+public @interface CHDConfig {
+
+    @AttributeDefinition(name = "Site Page Path", type = AttributeType.STRING)
+    String contentRootPath() default "/content/chd";
+
+   @AttributeDefinition(name = "Asset Page Path", type = AttributeType.STRING)
+    String assetRootPath() default "/content/dam/chd";
+
+}
